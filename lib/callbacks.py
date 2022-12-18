d832f9524f7a8c677db2b2b5c0ac1256d6d9ccf7
Mikubill <31246794+Mikubill@users.noreply.github.com>
2022-12-18 15:08:33 +0000

    feat: add sampling callback

--- lib/callbacks.py
+++ lib/callbacks.py
@@ -1,7 +1,64 @@
 import tempfile
 from huggingface_hub import Repository
 from huggingface_hub.constants import ENDPOINT
+from omegaconf import OmegaConf
 from pytorch_lightning import Callback
+
+
+import torch
+from pathlib import Path
+from pytorch_lightning.utilities import rank_zero_only
+
+class SampleCallback(Callback):
+    def __init__(self, config):
+        self.config = config    
+        
+    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):        
+        if self.config is None or pl_module.pipeline is None or self.config.every_n_steps == -1:
+            return super().on_train_batch_end(trainer, pl_module, outputs, batch, batch_idx)
+        
+        if trainer.global_step % self.config.every_n_steps == 0 and trainer.global_step > 0:
+            return self.sample(trainer, pl_module.pipeline)
+        
+    def on_train_epoch_end(self, trainer, pl_module):
+        if self.config is None or pl_module.pipeline is None or self.config.every_n_epochs == -1:
+            return super().on_train_epoch_end(trainer, pl_module)
+        
+        if trainer.current_epoch % self.config.every_n_epochs == 0:
+            return self.sample(trainer, pl_module.pipeline)
+        
+    @torch.inference_mode()
+    @rank_zero_only 
+    def sample(self, trainer, pipeline):
+        if not any(self.config.prompts):
+            return
+        
+        save_dir = Path(self.config.save_dir) 
+        save_dir.mkdir(parents=True, exist_ok=True)
+        generator = torch.Generator(device=pipeline.device).manual_seed(self.config.seed)
+        
+        negative_prompts = list(self.config.negative_prompts) if OmegaConf.is_list(self.config.negative_prompts) else self.config.negative_prompts
+        prompts = list(self.config.prompts) if OmegaConf.is_list(self.config.prompts) else self.config.prompts
+        images = pipeline(
+            prompt=prompts,
+            height=self.config.height,
+            width=self.config.width,
+            num_inference_steps=self.config.steps,
+            guidance_scale=self.config.cfg_scale,
+            negative_prompt=negative_prompts,
+            generator=generator,
+        ).images
+
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+
+        for j, image in enumerate(images):
+            image.save(save_dir / f"nd_sample_e{trainer.current_epoch}_s{trainer.global_step}_{j}.png")
+        
+        if self.config.use_wandb:
+            import wandb
+            samples = [wandb.Image(image, caption=self.config.prompts[j]) for j, image in enumerate(images)]
+            wandb.log({"samples": samples}, trainer.global_step)
 
 # Modified: https://github.com/nateraw/hf-hub-lightning/blob/main/hf_hub_lightning/callback.py
 
