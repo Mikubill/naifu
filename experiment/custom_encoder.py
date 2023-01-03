fix: remove trainer.precision param to avoid nan

experiment/custom_encoder.py
@@ -29,7 +29,7 @@ class CustomEncoderDiffusionModel(StableDiffusionModel):
         self.vae = AutoencoderKL.from_pretrained(self.model_path, subfolder="vae")
         self.unet = UNet2DConditionModel.from_pretrained(self.model_path, subfolder="unet") 
              
-        self.unet.to(torch.float16 if config.trainer.precision == "fp16" else torch.float32)
+        self.unet.to(torch.float32)
         if config.trainer.half_encoder:
             self.vae.to(torch.float16)
 