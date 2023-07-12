54c0c71f925ca72b1733690786579ba4933e788c
Mikubill <31246794+Mikubill@users.noreply.github.com>
2023-07-12 16:05:33 +0000

    quickfix

--- lib/diffusers_convert.py
+++ lib/diffusers_convert.py
@@ -149,6 +149,10 @@ vae_conversion_map_attn = [
     ("k.", "key."),
     ("v.", "value."),
     ("proj_out.", "proj_attn."),
+    ("q.", "to_q."),
+    ("k.", "to_k."),
+    ("v.", "to_v."),
+    ("proj_out.", "to_out.0."),
 ]
 
 
@@ -163,6 +167,7 @@ def convert_vae_state_dict(vae_state_dict):
         for sd_part, hf_part in vae_conversion_map:
             v = v.replace(hf_part, sd_part)
         mapping[k] = v
+        
     for k, v in mapping.items():
         if "attentions" in k:
             for sd_part, hf_part in vae_conversion_map_attn:
@@ -170,9 +175,10 @@ def convert_vae_state_dict(vae_state_dict):
             mapping[k] = v
     new_state_dict = {v: vae_state_dict[k] for k, v in mapping.items()}
     weights_to_convert = ["q", "k", "v", "proj_out"]
+    
     for k, v in new_state_dict.items():
         for weight_name in weights_to_convert:
-            if f"mid.attn_1.{weight_name}.weight" in k:
+            if f"mid.attn_1.{weight_name}.weight" in k: 
                 # print(f"Reshaping {k} for SD format")
                 new_state_dict[k] = reshape_weight_for_sd(v)
     return new_state_dict
