6ffc7570b7f0833641de71206e9056bcb6b2d7e5
Mikubill <31246794+Mikubill@users.noreply.github.com>
2023-07-18 05:49:24 +0000

    quickfix: add tool for extracting lora

--- experiment/extract_lora.py
+++ experiment/extract_lora.py
@@ -1,17 +1,43 @@
 import argparse
 import torch
+from pathlib import Path
+from safetensors.torch import save_file
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--src", default=None, type=str, required=True, help="Path to the model to convert.")
-    parser.add_argument("--dst", default=None, type=str, required=True, help="Path to the output model.")
-    args = parser.parse_args()
-
-    assert args.src is not None, "Must provide a model path!"
-    assert args.dst is not None, "Must provide a checkpoint path!"
-
-    state_dict = torch.load(args.src, map_location="cpu")
+def process_file(src, dst, file_type):
+    state_dict = torch.load(src, map_location="cpu")
     state_dict = state_dict["state_dict"] if "state_dict" in state_dict else state_dict
     lorasd = {k.replace("lora.", ""): v for k, v in state_dict.items()}
     
-    torch.save(lorasd, args.dst)
+    print(f"Saving to {dst}")
+    if file_type == "pt":
+        torch.save(lorasd, dst)
+    elif file_type == "safetensors":
+        save_file(lorasd, dst)
+    else:
+        raise ValueError("Invalid file_type. It must be 'pt' or 'safetensors'")
+
+def process_directory(src, dst=None, file_type="pt"):
+    src_path = Path(src)
+    if dst:
+        dst_path = Path(dst)
+    else:
+        dst_path = src_path if src_path.is_dir() else src_path.parent
+
+    if src_path.is_file() and src_path.suffix == '.ckpt':
+        dst_file = dst_path / (src_path.stem + '.' + file_type)
+        process_file(src_path, dst_file, file_type)
+    else:
+        for pt_file in src_path.rglob('*.ckpt'):
+            dst_file = dst_path / (pt_file.stem + '.' + file_type)
+            process_file(pt_file, dst_file, file_type)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--src", default=None, type=str, required=True, help="Path to the file or directory to process.")
+    parser.add_argument("--dst", default=None, type=str, help="Path to the destination file or directory.")
+    parser.add_argument("--type", default="pt", type=str, choices=["pt", "safetensors"], help="File type to save as.")
+    args = parser.parse_args()
+
+    assert args.src is not None, "Must provide a source path!"
+
+    process_directory(args.src, args.dst, args.type)
