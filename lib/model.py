8ce0bd5d592fe60763a1cc9910c653512ee3b4ee
Mikubill <31246794+Mikubill@users.noreply.github.com>
2023-09-07 19:29:44 +0000

    minor fix

--- lib/model.py
+++ lib/model.py
@@ -244,7 +244,7 @@ class StableDiffusionModel(pl.LightningModule):
             cond = batch["conds"]
 
         # Sample noise that we'll add to the latents
-        noise = torch.randn_like(latents)
+        noise = torch.randn_like(latents, dtype=self.model.dtype)
         if self.config.trainer.get("offset_noise"):
             noise = torch.randn_like(latents) + float(self.config.trainer.get("offset_noise_val")) \
                 * torch.randn(latents.shape[0], latents.shape[1], 1, 1, device=latents.device)
