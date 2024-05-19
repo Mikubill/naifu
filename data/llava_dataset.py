2dfd0e1b2e989123212a37e8478c8b20bc3a78b9
Mikubill <31246794+Mikubill@users.noreply.github.com>
2024-05-19 04:42:13 +0000

    minor update: dataset.py, utils.py

--- data/llava_dataset.py
+++ data/llava_dataset.py
@@ -1,7 +1,7 @@
 # copy from https://github.com/haotian-liu/LLaVA/blob/7440ec9ee37b0374c6b5548818e89878e38f3353/llava/train/train.py
 
 import copy
-from models.llava.mm_utils import process_images, tokenizer_image_token
+from models.llava.mm_utils import process_anyres_image, tokenizer_image_token
 from models.llava.constants import *
 from typing import Dict
 from torch.utils.data import Dataset
@@ -14,7 +14,6 @@ import data.conversation as conv_lib
 from data.conversation import conv_templates
 from packaging import version
 import tokenizers
-from common.logging import logger
 from torch.nn.utils.rnn import pad_sequence
 IS_TOKENIZER_GREATER_THAN_0_14 = version.parse(tokenizers.__version__) >= version.parse('0.14')
 
@@ -28,8 +27,8 @@ class LazySupervisedDataset(Dataset):
         image_folder, 
         tokenizer, 
         mm_config, 
-        image_processor = None, 
-        version: str = "v1", 
+        image_processor, 
+        version="v1", 
         model_max_length: int = 512,
         is_multimodel: bool = True,
         **kwargs
@@ -40,8 +39,8 @@ class LazySupervisedDataset(Dataset):
         self.tokenizer = tokenizer
         self.mm_config = mm_config
         if hasattr(self.mm_config, "image_grid_pinpoints"):
-            self.mm_config.image_grid_pinpoints = list(mm_config.image_grid_pinpoints)
-        
+             self.mm_config.image_grid_pinpoints = list(mm_config.image_grid_pinpoints)
+             
         self.list_data_dict = list_data_dict
         self.image_folder = image_folder
         self.image_processor = image_processor
@@ -73,13 +72,32 @@ class LazySupervisedDataset(Dataset):
         if isinstance(i, int):
             sources = [sources]
         assert len(sources) == 1, "Don't know why it is wrapped to a list"  # FIXME
-        
         if 'image' in sources[0]:
             image_file = self.list_data_dict[i]['image']
             image_folder = self.image_folder
+            processor = self.image_processor
             image = Image.open(os.path.join(image_folder, image_file)).convert('RGB')
             image_size = image.size
-            image = process_images([image], self.image_processor, self.mm_config)[0]
+            if self.mm_config.image_aspect_ratio == 'pad':
+                def expand2square(pil_img, background_color):
+                    width, height = pil_img.size
+                    if width == height:
+                        return pil_img
+                    elif width > height:
+                        result = Image.new(pil_img.mode, (width, width), background_color)
+                        result.paste(pil_img, (0, (width - height) // 2))
+                        return result
+                    else:
+                        result = Image.new(pil_img.mode, (height, height), background_color)
+                        result.paste(pil_img, ((height - width) // 2, 0))
+                        return result
+                image = expand2square(image, tuple(int(x*255) for x in processor.image_mean))
+                image = processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
+            elif self.mm_config.image_aspect_ratio == 'anyres':
+                image = process_anyres_image(image, processor, self.mm_config.image_grid_pinpoints)
+            else:
+                image = processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
+                
             sources = self.preprocess_multimodal(copy.deepcopy([e["conversations"] for e in sources]))
         else:
             sources = copy.deepcopy([e["conversations"] for e in sources])
@@ -96,7 +114,6 @@ class LazySupervisedDataset(Dataset):
             data_dict['image'] = image
             data_dict['image_size'] = image_size
         elif self.is_multimodal:
-            logger.warn(f"Image does not exist in the data, but the model is multimodal. Filling with zeros.")
             # image does not exist in the data, but the model is multimodal
             crop_size = self.image_processor.crop_size
             data_dict['image'] = torch.zeros(3, crop_size['height'], crop_size['width'])
@@ -180,7 +197,6 @@ class LazySupervisedDataset(Dataset):
 
     def preprocess_plain(self, sources):
         tokenizer = self.tokenizer
-        
         # add end signal and concatenate together
         conversations = []
         for source in sources:
@@ -189,7 +205,6 @@ class LazySupervisedDataset(Dataset):
             source[0]['value'] = DEFAULT_IMAGE_TOKEN
             conversation = source[0]['value'] + source[1]['value'] + self.default_conv.sep
             conversations.append(conversation)
-            
         # tokenize conversations
         input_ids = [tokenizer_image_token(prompt, tokenizer, return_tensors='pt') for prompt in conversations]
         targets = copy.deepcopy(input_ids)
@@ -211,21 +226,28 @@ class LazySupervisedDataset(Dataset):
         """
         if self.default_conv.sep_style == conv_lib.SeparatorStyle.PLAIN:
             return self.preprocess_plain(sources)
-        if self.default_conv.version.startswith("v1") or self.default_conv.sep_style == conv_lib.SeparatorStyle.LLAMA_2:
+        if self.default_conv.sep_style == conv_lib.SeparatorStyle.LLAMA_2:
+            return self.preprocess_llama_2(sources, has_image=has_image)
+        if self.default_conv.sep_style == conv_lib.SeparatorStyle.MPT:
+            return self.preprocess_mpt(sources)
+        if self.default_conv.version.startswith("v1"):
             return self.preprocess_v1(sources, has_image=has_image)
-        
         # add end signal and concatenate together
         conversations = []
         for source in sources:
             header = f"{self.default_conv.system}\n\n"
             conversation = self._add_speaker_and_signal(header, source)
             conversations.append(conversation)
-            
         # tokenize conversations
         def get_tokenize_len(prompts):
             return [len(tokenizer_image_token(prompt, tokenizer)) for prompt in prompts]
 
-        input_ids = self.tokenize_conversations(conversations, has_image, tokenizer)
+        if has_image:
+            input_ids = [tokenizer_image_token(prompt, tokenizer, return_tensors='pt') for prompt in conversations]
+        else:
+            conversations_tokenized = self._tokenize_fn(conversations)
+            input_ids = conversations_tokenized["input_ids"]
+
         targets = copy.deepcopy(input_ids)
         for target, source in zip(targets, sources):
             if has_image:
@@ -237,7 +259,7 @@ class LazySupervisedDataset(Dataset):
 
         return dict(input_ids=input_ids, labels=targets)
 
-    def preprocess_v1(self, sources, has_image: bool = False):        
+    def preprocess_llama_2(self, sources, has_image: bool = False):        
         tokenizer = self.tokenizer
         conv = self.default_conv.copy()
         roles = {"human": conv.roles[0], "gpt": conv.roles[1]}
@@ -245,8 +267,8 @@ class LazySupervisedDataset(Dataset):
         # Apply prompt templates
         conversations = []
         for i, source in enumerate(sources):
-            # Skip the first one if it is not from human
             if roles[source[0]["from"]] != conv.roles[0]:
+                # Skip the first one if it is not from human
                 source = source[1:]
 
             conv.messages = []
@@ -257,20 +279,26 @@ class LazySupervisedDataset(Dataset):
             conversations.append(conv.get_prompt())
 
         # Tokenize conversations
-        input_ids = self.tokenize_conversations(conversations, has_image, tokenizer)
-        targets = copy.deepcopy(input_ids)
 
+        if has_image:
+            input_ids = torch.stack([tokenizer_image_token(prompt, tokenizer, return_tensors='pt') for prompt in conversations], dim=0)
+        else:
+            input_ids = tokenizer(
+                conversations,
+                return_tensors="pt",
+                padding="longest",
+                max_length=tokenizer.model_max_length,
+                truncation=True,
+            ).input_ids
+
+        targets = input_ids.clone()
+
+        assert conv.sep_style == conv_lib.SeparatorStyle.LLAMA_2
         # Mask targets
-        match conv.sep_style:
-            case conv_lib.SeparatorStyle.LLAMA_2: 
-                sep = "[/INST] "
-            case conv_lib.SeparatorStyle.TWO:
-                sep = conv.sep + conv.roles[1] + ": "
-            case _:
-                raise ValueError("Unsupported separator style")
-            
+        sep = "[/INST] "
         for conversation, target in zip(conversations, targets):
             total_len = int(target.ne(tokenizer.pad_token_id).sum())
+
             rounds = conversation.split(conv.sep2)
             cur_len = 1
             target[:cur_len] = IGNORE_INDEX
@@ -282,45 +310,174 @@ class LazySupervisedDataset(Dataset):
                 if len(parts) != 2:
                     break
                 parts[0] += sep
-                round_len, instruction_len = self.get_lengths(rou, parts[0], has_image, tokenizer, i)
 
-                # Mask the instruction part of the target
+                if has_image:
+                    round_len = len(tokenizer_image_token(rou, tokenizer))
+                    instruction_len = len(tokenizer_image_token(parts[0], tokenizer)) - 2
+                else:
+                    round_len = len(tokenizer(rou).input_ids)
+                    instruction_len = len(tokenizer(parts[0]).input_ids) - 2
+
                 target[cur_len : cur_len + instruction_len] = IGNORE_INDEX
+
                 cur_len += round_len
-            
-            # Check for tokenization mismatch
             target[cur_len:] = IGNORE_INDEX
+
             if cur_len < tokenizer.model_max_length:
                 if cur_len != total_len:
                     target[:] = IGNORE_INDEX
-                    logger.warning(f"tokenization mismatch: {cur_len} vs. {total_len}. (ignored)")
+                    print(
+                        f"WARNING: tokenization mismatch: {cur_len} vs. {total_len}."
+                        f" (ignored)"
+                    )
 
         return dict(
             input_ids=input_ids,
             labels=targets,
         )
+    
+    def preprocess_v1(self, sources, has_image: bool = False):        
+        tokenizer = self.tokenizer
+        conv = self.default_conv.copy()
+        roles = {"human": conv.roles[0], "gpt": conv.roles[1]}
 
-    def tokenize_conversations(self, conversations, has_image, tokenizer):
+        # Apply prompt templates
+        conversations = []
+        for i, source in enumerate(sources):
+            if roles[source[0]["from"]] != conv.roles[0]:
+                # Skip the first one if it is not from human
+                source = source[1:]
+
+            conv.messages = []
+            for j, sentence in enumerate(source):
+                role = roles[sentence["from"]]
+                assert role == conv.roles[j % 2], f"{i}"
+                conv.append_message(role, sentence["value"])
+            conversations.append(conv.get_prompt())
+
+        # Tokenize conversations
         if has_image:
-            return torch.stack([tokenizer_image_token(prompt, tokenizer, return_tensors='pt') for prompt in conversations], dim=0)
+            input_ids = torch.stack([tokenizer_image_token(prompt, tokenizer, return_tensors='pt') for prompt in conversations], dim=0)
         else:
-            conversations_tokenized = self._tokenize_fn(conversations)
-            input_ids = conversations_tokenized["input_ids"]
+            input_ids = tokenizer(
+                conversations,
+                return_tensors="pt",
+                padding="longest",
+                max_length=tokenizer.model_max_length,
+                truncation=True,
+            ).input_ids
 
-    def get_lengths(self, rou, part, has_image, tokenizer, i):
-        if has_image:
-            round_len = len(tokenizer_image_token(rou, tokenizer))
-            instruction_len = len(tokenizer_image_token(part, tokenizer)) - 2
-        else:
-            round_len = len(tokenizer(rou).input_ids)
-            instruction_len = len(tokenizer(part).input_ids) - 2
+        targets = input_ids.clone()
 
-        if self.default_conv.version.startswith("v1"):
-            if i != 0 and not tokenizer.legacy and IS_TOKENIZER_GREATER_THAN_0_14:
-                round_len -= 1
-                instruction_len -= 1
+        assert conv.sep_style == conv_lib.SeparatorStyle.TWO
 
-        return round_len, instruction_len
+        # Mask targets
+        sep = conv.sep + conv.roles[1] + ": "
+        for conversation, target in zip(conversations, targets):
+            total_len = int(target.ne(tokenizer.pad_token_id).sum())
+
+            rounds = conversation.split(conv.sep2)
+            cur_len = 1
+            target[:cur_len] = IGNORE_INDEX
+            for i, rou in enumerate(rounds):
+                if rou == "":
+                    break
+
+                parts = rou.split(sep)
+                if len(parts) != 2:
+                    break
+                parts[0] += sep
+
+                if has_image:
+                    round_len = len(tokenizer_image_token(rou, tokenizer))
+                    instruction_len = len(tokenizer_image_token(parts[0], tokenizer)) - 2
+                else:
+                    round_len = len(tokenizer(rou).input_ids)
+                    instruction_len = len(tokenizer(parts[0]).input_ids) - 2
+
+                if i != 0 and not tokenizer.legacy and IS_TOKENIZER_GREATER_THAN_0_14:
+                    round_len -= 1
+                    instruction_len -= 1
+
+                target[cur_len : cur_len + instruction_len] = IGNORE_INDEX
+
+                cur_len += round_len
+            target[cur_len:] = IGNORE_INDEX
+
+            if cur_len < tokenizer.model_max_length:
+                if cur_len != total_len:
+                    target[:] = IGNORE_INDEX
+                    print(
+                        f"WARNING: tokenization mismatch: {cur_len} vs. {total_len}."
+                        f" (ignored)"
+                    )
+
+        return dict(
+            input_ids=input_ids,
+            labels=targets,
+        )
+        
+    def preprocess_mpt(self, sources) -> Dict:
+        conv = self.default_conv.copy()
+        roles = {"human": conv.roles[0], "gpt": conv.roles[1]}
+
+        # Apply prompt templates
+        conversations = []
+        for i, source in enumerate(sources):
+            if roles[source[0]["from"]] != conv.roles[0]:
+                # Skip the first one if it is not from human
+                source = source[1:]
+
+            conv.messages = []
+            for j, sentence in enumerate(source):
+                role = roles[sentence["from"]]
+                assert role == conv.roles[j % 2], f"{i}"
+                conv.append_message(role, sentence["value"])
+            conversations.append(conv.get_prompt())
+
+        # Tokenize conversations
+        input_ids = torch.stack([tokenizer_image_token(prompt, self.tokenizer, return_tensors='pt') for prompt in conversations], dim=0)
+        targets = input_ids.clone()
+        assert conv.sep_style == conv_lib.SeparatorStyle.MPT
+
+        # Mask targets
+        sep = conv.sep + conv.roles[1]
+        for conversation, target in zip(conversations, targets):
+            total_len = int(target.ne(self.tokenizer.pad_token_id).sum())
+
+            rounds = conversation.split(conv.sep)
+            re_rounds = [conv.sep.join(rounds[:3])] # system + user + gpt
+            for conv_idx in range(3, len(rounds), 2):
+                re_rounds.append(conv.sep.join(rounds[conv_idx:conv_idx+2]))    # user + gpt
+            cur_len = 0
+            target[:cur_len] = IGNORE_INDEX
+            for i, rou in enumerate(re_rounds):
+                if rou == "":
+                    break
+
+                parts = rou.split(sep)
+                if len(parts) != 2:
+                    break
+                parts[0] += sep
+                round_len = len(tokenizer_image_token(rou, self.tokenizer)) + len(tokenizer_image_token(conv.sep, self.tokenizer))
+                instruction_len = len(tokenizer_image_token(parts[0], self.tokenizer))
+                target[cur_len : cur_len + instruction_len] = IGNORE_INDEX
+
+                cur_len += round_len
+            target[cur_len:] = IGNORE_INDEX
+
+            if cur_len < self.model_max_length:
+                if cur_len != total_len:
+                    target[:] = IGNORE_INDEX
+                    print(
+                        f"WARNING: tokenization mismatch: {cur_len} vs. {total_len}."
+                        f" (ignored)"
+                    )
+
+        return dict(
+            input_ids=input_ids,
+            labels=targets,
+        )
         
     def padding_collate_fn(self, instances):
         input_ids, labels = tuple([instance[key] for instance in instances] for key in ("input_ids", "labels"))
@@ -337,19 +494,19 @@ class LazySupervisedDataset(Dataset):
         if 'image' in instances[0]:
             images = [instance['image'] for instance in instances]
             batch['image_sizes'] = [instance['image_size'] for instance in instances]
+            
             if all(x is not None and x.shape == images[0].shape for x in images):
                 batch['images'] = torch.stack(images)
             else:
                 batch['images'] = images
-                
         return batch
                 
     def build_dataloader(self, batch_size: int, shuffle: bool = False):
         dataloader = torch.utils.data.DataLoader(
             self, 
             batch_size=batch_size, 
-            shuffle=shuffle, 
             num_workers=8,
+            shuffle=shuffle, 
             collate_fn=self.padding_collate_fn
         )
         return dataloader
